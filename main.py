a628e8abfb90b47027e7f11b98075b3a717c7934
MUDASSARHASHMI <mudassarziahashmim@hotmail.com>
2025-11-15 19:45:16 +0500

    limiting with enum

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+from enum import Enum
 from fastapi import FastAPI
 
 app = FastAPI()
+
 @app.get("/")
 async def hello_world():
     return {"Hello": "World"}
+
+# Dynamic Parameters for Path
+
+# @app.get("/users/{id}")
+# async def get_users(id: int):
+#     return {"id": id }
+
+@app.get("/users/{type}/{id}")
+async def get_user(type: str, id: int):
+    return {"type": type, "id":id}
